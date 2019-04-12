b059376c53a8aafe25e35774ea2d08baca8e754c
iamaureen <ishrat.ahmed@asu.edu>
2019-04-12 00:50:40 -0400

    badge hover persistent,badge close,dynamic badge description,badge example added

--- textbook/app/static/js/awardbanner.js
+++ textbook/app/static/js/awardbanner.js
@@ -48,8 +48,12 @@ function displayAllBadges(){
     }
 
     for(var key in dict){
+        var divBnrHeader = $('<div style="margin-left:150px;"></div>');
+        $(src).append(divBnrHeader);
+
         var divBnr = $('<div style="float:left"></div>');
-        $(src).append(divBnr);
+        $(divBnrHeader).append(divBnr)
+
 
         var spandiv = $('<span class="imgtooltip"></span>');
         $(divBnr).append(spandiv)
@@ -79,31 +83,44 @@ function displayAllBadges(){
 
   }
 
+var badge_dict_description = {'suggestion': 'Suggestion Description', 'social' : 'Good Citizen Description', 'relevance' : 'Relevant Post Description',
+'reflection' : 'Good Communication Description', 'ques' : 'Question Description', 'feedback' : 'Feedback Description',
+'explanation' : 'Good Explanation description', 'cocon': 'Co-Construction description'};
+
+var badge_dict_example = {'suggestion': 'Suggestion Example', 'social' : 'Good Citizen Example', 'relevance' : 'Relevant Post Example',
+'reflection' : 'Good Communication Example', 'ques' : 'Question Example', 'feedback' : 'Feedback Example',
+'explanation' : 'Good Explanation Example', 'cocon': 'Co-Construction Example'};
 
   function hoverBadge(){
 
    for(var key in dict){
 
-        $("img#"+key).on("mouseover", function () {
+        //$("img#"+key).on("mouseover", function () {
+        $("img#"+key).on("mouseenter", function () {
              //stuff to do on mouseover
              //alert('here') //works
              //$('#badge-description').children('img').remove();
-               $('#badge-description').empty();
+             $('#badge-description').show();
+              $('#badge-description').empty();
              var display = $(this).attr('id');
              var badge_descript = $('#badge-description');
 
+            $('#badge-description').append("<span onclick=this.parentElement.style.display='none' class='topright'>&times</span>")
 
              var img = $('<img/>', { id: display, src : 'http://'+ host_url + "/static/pics/" + display + ".png" }).css({"width":"50px", "margin-right": "5px", "margin-left": "5px"}).appendTo(badge_descript);
 
              $('#badge-description').append("<p id = 'badge_name'>" + badge_dict[display] + "</p>");
-             $('#badge-description').append("<p id = 'badge_descrip'>This will be a short description on how you can earn this badge.</p>");
+
+             $('#badge-description').append("<p id = 'badge_descrip'>"+badge_dict_description[display]+".</p>");
+
+             $('#badge-description').append("<p id = 'badge_example'>"+badge_dict_example[display]+".</p>");
 
              $('#badge-description').css('opacity','1');
              console.log(key);
 
 
         }).on("mouseout", function(){
-            $('#badge-description').css('opacity','0');
+            //$('#badge-description').css('opacity','0');
 
         });
     }
